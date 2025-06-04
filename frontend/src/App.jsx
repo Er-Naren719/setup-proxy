b55c343bfa9cda4821769c21eb97a0cea705ba50
Narendra Patil <patilnarendra3@gmail.com>
2025-06-04 11:17:43 +0530

    avoid race condition using axios controller

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
-import useCustomReactQuery from "./hooks/useCustomReactQuery";
+// import useCustomReactQuery from "./hooks/useCustomReactQuery";
 
 function App() {
 	// const [jokes, setJokes] = useState([]);
@@ -18,15 +18,48 @@ function App() {
 	// 		});
 	// }, []);
 
-	const { products, error, loading } = useCustomReactQuery("/api/products");
+	// const { products, error, loading } = useCustomReactQuery("/api/products");
 
-	if (loading) {
-		return <p>Loading...</p>;
-	}
+	const [products, setProducts] = useState([]);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [search, setSearch] = useState("");
 
-	if (error) {
-		return <p>Error fetching data. Please try again later.</p>;
-	}
+	useEffect(() => {
+		const controller = new AbortController();
+		(async () => {
+			try {
+				setLoading(true);
+				setError(false);
+				const response = await axios.get(`/api/products?search=${search}`, {
+					signal: controller.signal,
+				});
+				console.log(response.data);
+				setProducts(response.data);
+				setLoading(false);
+			} catch (error) {
+				if (axios.isCancel(error)) {
+					console.log("Request canceled:", error.message);
+					return;
+				}
+				setError(true);
+				setLoading(false);
+				console.error("Error fetching products:", error);
+			}
+		})();
+
+		return () => {
+			controller.abort();
+		};
+	}, [search]);
+
+	// if (loading) {
+	// 	return <p>Loading...</p>;
+	// }
+
+	// if (error) {
+	// 	return <p>Error fetching data. Please try again later.</p>;
+	// }
 
 	return (
 		<>
@@ -37,7 +70,15 @@ function App() {
 					<p>{joke.joke}</p>
 				</div>
 			))} */}
-			<p>Total products are: {products.length}</p>
+			<input
+				type="text"
+				placeholder="Search products..."
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			{loading && <p>Loading products...</p>}
+			{error && <p>Error fetching products. Please try again later.</p>}
+			{!loading && !error && <p>Total products are: {products.length}</p>}
 		</>
 	);
 }
